cb0723f1098167714534d5bd7e36a00820bc7d61
Sene Conté <sceee@iscte-iul.pt>
2020-03-23 10:40:48 +0000

    Junit

--- Project/src/test/java/Project_ES2/Project/AllTests.java
+++ Project/src/test/java/Project_ES2/Project/AllTests.java
@@ -1,16 +1,13 @@
 package Project_ES2.Project;
 
-import junit.framework.Test;
-import junit.framework.TestSuite;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.runner.RunWith;
 
+
+@RunWith(JUnitPlatform.class)
+@SelectClasses({TestApp.class})
 public class AllTests {
 
-	public static Test suite() {
-		TestSuite suite = new TestSuite(AllTests.class.getName());
-		//$JUnit-BEGIN$
-		suite.addTestSuite(TestApp.class);
-		//$JUnit-END$
-		return suite;
-	}
-
 }
+
